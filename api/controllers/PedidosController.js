04b49b6e13463f2819b3d7309bbb0a06668139f9
NivekSotam <kevin.matoss.km@gmail.com>
2022-06-22 20:16:56 -0300

    Selecionar pedidos

--- api/controllers/PedidosController.js
+++ api/controllers/PedidosController.js
@@ -2,10 +2,10 @@ const { BOOLEAN } = require('sequelize');
 const database = require('../models');
 
 class PedidosController {
-    
-    static async criaPedido(req, res){
-        const {mesaId, funcionarioId, clienteId, fechado } = req.body;
-        const novoPedido = {mesa_id: Number(mesaId), funcionario_id: Number(funcionarioId), cliente_id: Number(clienteId)} ;
+
+    static async criaPedido(req, res) {
+        const { mesaId, funcionarioId, clienteId, fechado } = req.body;
+        const novoPedido = { mesa_id: Number(mesaId), funcionario_id: Number(funcionarioId), cliente_id: Number(clienteId) };
         try {
             const novoPedidoCriado = await database.Pedidos.create(novoPedido);
             return res.status(200).json(novoPedidoCriado);
@@ -14,70 +14,95 @@ class PedidosController {
         }
     }
 
-    static async adicionarItemPedido(req, res){
+    static async adicionarItemPedido(req, res) {
         const { pedido_id } = req.params;
         const { items } = req.body;
-        try{
-        for (const item of items) {
+        try {
+            for (const item of items) {
 
-            const itemDb = await database.Itens.findByPk(item.itemId);
-            console.log(itemDb.dataValues);
-            const itemPedido = {
-                itens_id: item.itemId,
-                pedido_id: pedido_id,
-                quantidade: item.quantidade,
-                valor_itens: itemDb.dataValues.valor
-            };
-            const itemObj = await database.itens_pedido.findOne({
-                where: {
+                const itemDb = await database.Itens.findByPk(item.itemId);
+                console.log(itemDb.dataValues);
+                const itemPedido = {
                     itens_id: item.itemId,
-                    pedido_id: pedido_id 
-                }
-            })
-            console.log(itemObj)
-            if (itemObj){
-
-                let quantidade;
-                if (item.acao === "ADICIONAR") {
-                    quantidade = itemObj.dataValues.quantidade + item.quantidade
-                } else {
-                    quantidade = itemObj.dataValues.quantidade - item.quantidade
-                }
-                await database.itens_pedido.update({
-                    quantidade: quantidade,
+                    pedido_id: pedido_id,
+                    quantidade: item.quantidade,
                     valor_itens: itemDb.dataValues.valor
-                },
-                {
-                    where:{
-                        pedido_id: pedido_id,
-                        itens_id: itemObj.itens_id
+                };
+                const itemObj = await database.itens_pedido.findOne({
+                    where: {
+                        itens_id: item.itemId,
+                        pedido_id: pedido_id
                     }
                 })
-            } else {
-                await database.itens_pedido.create(itemPedido)
+                console.log(itemObj)
+                if (itemObj) {
+
+                    let quantidade;
+                    if (item.acao === "ADICIONAR") {
+                        quantidade = itemObj.dataValues.quantidade + item.quantidade
+                    } else {
+                        quantidade = itemObj.dataValues.quantidade - item.quantidade
+                    }
+                    await database.itens_pedido.update({
+                        quantidade: quantidade,
+                        valor_itens: itemDb.dataValues.valor
+                    },
+                        {
+                            where: {
+                                pedido_id: pedido_id,
+                                itens_id: itemObj.itens_id
+                            }
+                        })
+                } else {
+                    await database.itens_pedido.create(itemPedido)
+                }
             }
-        }
-        return res.status(200).json(items)
+            return res.status(200).json(items)
         } catch (err) {
             return res.status(500).json(err.message);
         }
     }
 
-    static async pegaValorTotal(req, res){
+    static async pegaValorTotal(req, res) {
         const { pedido_id } = req.params;
-        
-        try{
-            const valorTotal = await database.itens_pedido.sum('valor_itens',{
+
+        try {
+            const valorTotal = await database.itens_pedido.sum('valor_itens', {
                 where: {
                     pedido_id: pedido_id
                 },
             })
             return res.status(200).json(valorTotal)
-        } catch(err) {
+        } catch (err) {
             return res.status(500).json(err.message)
         }
-    }    
-    
+    }
+
+
+    static async pegarTodosOsPedidos(req, res) {
+        try {
+            const todosOsPedidos = await database.itens_pedido.findAll()
+            return res.status(200).json( todosOsPedidos)
+        } catch (err) {
+            return res.status(500).json(err.message)
+        }
+    }
+
+    static async pegaUmPedido(req, res) {
+        const { id } = req.params;
+        try {
+            const umPedido = await database.itens_pedido.findOne({
+                where: {
+                   id: Number(id)
+                }
+            })
+            return res.status(200).json(umPedido);
+        } catch (err) {
+            return res.status(500).json(err.message);
+        }
+    }
 }
+
+
 
 module.exports = PedidosController;
--- api/routes/pedidosRoute.js
+++ api/routes/pedidosRoute.js
@@ -7,5 +7,7 @@ router.post('/pedidos', PedidosController.criaPedido);
 
 router.post('/pedidos/:pedido_id', PedidosController.adicionarItemPedido)
 router.get('/pedidos/:pedido_id', PedidosController.pegaValorTotal)
+router.get('/pedidos', PedidosController.pegarTodosOsPedidos);
+router.get('/pedido/:id', PedidosController. pegaUmPedido);
 
 module.exports = router;
